33057809e272bc9b88284968f4687116bfeb8083
vinoth <vkpselvam0511@gmail.com>
2023-07-10 18:09:59 +0530

    1st commit updated

--- basics/Example1.py
+++ basics/Example1.py
@@ -2,14 +2,14 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-s=Service(executable_path="C:\\Users\\MyPc\\Documents\\Software Testing\\Selenium_Drivers\\chromedriver_win32\\chromedriver.exe")
-driver=webdriver.Chrome(service=s)
-driver.get("http://www.facebook.com")
-driver.maximize_window()
-time.sleep(5)
-driver.minimize_window()
-time.sleep(5)
-driver.maximize_window()
-time.sleep(5)
-driver.close()
+
+def test_open_chrome():
+    s=Service(ChromeDriverManager().install())
+    driver=webdriver.Chrome(service=s)
+    driver.get("http://www.facebook.com")
+    driver.maximize_window()
+    time.sleep(5)
+    assert driver.title=="Facebook – log in or sign up"
+    driver.close()
